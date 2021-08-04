9bd2b6388431f424eb43ec6a6404aa1d21a4bc5f
fitbouncer <bhuinjohn@gmail.com>
2021-08-04 20:46:14 +0530

    first commit

---
+++ app/Http/Controllers/USER/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\USER;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    //
+    public function index(){
+        return view('index');
+    }
+}
